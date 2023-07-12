f28af06e0e110b91fcbb6c087a2eb1a00344b2cc
Madhav Yadav <66372332+mymadhavyadav07@users.noreply.github.com>
2023-07-12 11:04:11 +0530

    Update default.js

--- components/context menus/default.js
+++ components/context menus/default.js
@@ -3,10 +3,10 @@ import React from 'react'
 function DefaultMenu(props) {
     return (
         <div id="default-menu" className={(props.active ? " block " : " hidden ") + " cursor-default w-52 context-menu-bg border text-left border-gray-900 rounded text-white py-4 absolute z-50 text-sm"}>
-            <a rel="noreferrer noopener" href="https://github.com/mymadhavyadav07/madhav.github.io" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
+            <a rel="noreferrer noopener" href="https://github.com/mymadhavyadav07/mymadhavyadav07.github.io" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
                 <span className="ml-5">🌟</span> <span className="ml-2">Star this Project</span>
             </a>
-            <a rel="noreferrer noopener" href="https://github.com/mymadhavyadav07/madhav.github.io/issues" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
+            <a rel="noreferrer noopener" href="https://github.com/mymadhavyadav07/mymadhavyadav07.github.io/issues" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
                 <span className="ml-5">❗</span> <span className="ml-2">Report bugs</span>
             </a>
             <Devider />
