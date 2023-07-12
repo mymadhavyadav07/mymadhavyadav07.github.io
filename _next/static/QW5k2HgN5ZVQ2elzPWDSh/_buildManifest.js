e29969bce8dbbb6c925f0013c2065513747f064f
mymadhavyadav07 <66372332+mymadhavyadav07@users.noreply.github.com>
2023-07-12 11:13:28 +0000

    Deploying to gh-pages from @ mymadhavyadav07/mymadhavyadav07.github.io@5d2c6670f20fdc0c7b05a9282547b6ea5130bfd4 🚀

--- _next/static/QW5k2HgN5ZVQ2elzPWDSh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-6cf11adc33c7fdd4.js","static/chunks/174-36811859e658affc.js","static/chunks/pages/index-21b5b3eebf732847.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
