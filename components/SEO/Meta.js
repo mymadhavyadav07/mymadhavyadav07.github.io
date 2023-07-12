ea6ad18e3e1174d4fcfa5a4433922b2bfdf3c5ed
Madhav Yadav <66372332+mymadhavyadav07@users.noreply.github.com>
2023-07-12 18:13:02 +0530

    Update Meta.js

--- components/SEO/Meta.js
+++ components/SEO/Meta.js
@@ -20,12 +20,12 @@ export default function Meta() {
             <meta name="theme-color" content="#E95420" />
 
             /* Search Engine */
-            <meta name="image" content="images/logos/fevicon.png" />
+            <meta name="image" content="./images/logos/fevicon.png" />
             /* Schema.org for Google */
             <meta itemProp="name" content="Madhav Yadav Portfolio - Cyber Security Researcher" />
             <meta itemProp="description"
                 content="Madhav Yadav's (mymadhavyadav07) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
-            <meta itemProp="image" content="images/logos/fevicon.png" />
+            <meta itemProp="image" content="./images/logos/fevicon.png" />
             /* Twitter */
             <meta name="twitter:card" content="summary" />
             <meta name="twitter:title" content="Madhav Yadav Portfolio - Cyber Security Researcher" />
@@ -33,7 +33,7 @@ export default function Meta() {
                 content="Madhav Yadav's (mymadhavyadav07) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
             <meta name="twitter:site" content="mymadhavyadav07" />
             <meta name="twitter:creator" content="mymadhavyadav07" />
-            <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
+            <meta name="twitter:image:src" content="./images/logos/logo_1024.png" />
             /* Open Graph general (Facebook, Pinterest & Google+) */
             <meta name="og:title" content="Madhav Yadav Portfolio - Cyber Security Researcher" />
             <meta name="og:description"
@@ -45,7 +45,7 @@ export default function Meta() {
             <meta name="og:type" content="website" />
 
             <link rel="icon" href="images/logos/fevicon.svg" />
-            <link rel="apple-touch-icon" href="images/logos/logo.png" />
+            <link rel="apple-touch-icon" href="./images/logos/logo.png" />
             <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
             <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
         </Head>
