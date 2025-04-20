c35eea7ca09903364dd791c38a2b19028da5a613
mymadhavyadav07 <mymadhavyadav07@gmail.com>
2025-04-20 11:09:17 +0530

    Updates

---
+++ _next/static/FOOOjwtWm1VLCFq6oeZy6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-6cf11adc33c7fdd4.js","static/chunks/861-94a45fd6ae4c0e66.js","static/chunks/pages/index-f647f8800f93491e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
